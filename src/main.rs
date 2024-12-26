8dce6982f126da2cc04f7e93f7f1b6a47f02dab6
Claire T <claire.tran23@gmail.com>
2024-12-26 23:57:33 +0700

    implement virtual machine

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
-use std::fs::File;
-use std::io::Read;
 use crate::code_generator::CodeGenerator;
 use crate::lexer::Lexer;
 use crate::optimizer::Optimizer;
 use crate::parser::Parser;
+use crate::virtual_machine::VirtualMachine;
+use std::{env, fs};
 
-mod lexer;
-mod parser;
-mod optimizer;
 mod code_generator;
+mod lexer;
+mod optimizer;
+mod parser;
+mod virtual_machine;
 
 fn main() {
-    let  file = File::open("./src/optimization_cases.txt");
-    let mut contents = String::new();
-    file.unwrap().read_to_string(&mut contents).unwrap();
+    let args: Vec<String> = env::args().collect();
 
-    println!("{}", contents);
-    println!("{}", contents.len());
+    if args.len() != 2 {
+        eprintln!("Usage: {} <source_file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let file_path = &args[1];
+
+    // Read the source file
+    let source_code = match fs::read_to_string(file_path) {
+        Ok(content) => content,
+        Err(error) => {
+            eprintln!("Error reading file {}: {}", file_path, error);
+            std::process::exit(1);
+        }
+    };
+
+    println!("==================SOURCE CODE===================");
+
+    println!("{}", source_code);
+    println!("{}", source_code.len());
+
     println!("==================RUN LEXICAL ANALYZE PHASE===================");
-    let tokens = Lexer::tokenize(&contents);
+    let tokens = Lexer::tokenize(&source_code);
 
     let t = tokens.iter();
     for to in t {
@@ -39,20 +57,20 @@ fn main() {
         }
     };
 
-
-
     println!("=================AFTER OPTIMIZE======================");
-    let optimized_expression =  Optimizer::optimize_ast(ast);
+    let optimized_expression = Optimizer::optimize_ast(ast);
     println!("{:#?}", optimized_expression);
 
     println!("====================CODE GENERATE=============");
     let mut code_generator = CodeGenerator::new();
     let opcodes = code_generator.generate(optimized_expression);
     let mut a = 0;
-    for op in opcodes {
-        println!("{} {:#?}",a, op);
+    for op in &opcodes {
+        println!("{} {:#?}", a, op);
         a = a + 1;
     }
 
-
+    println!("================VIRTUAL MACHINE====================");
+    let mut vm = VirtualMachine::new(opcodes);
+    vm.run();
 }
---
+++ src/virtual_machine.rs
@@ -0,0 +1,202 @@
+use crate::code_generator::OpCode;
+use std::collections::HashMap;
+
+pub struct VirtualMachine {
+    stack: Vec<i64>,
+    variables: HashMap<String, i64>,
+    instructions: Vec<OpCode>,
+    instruction_pointer: usize,
+    call_stack: Vec<usize>,
+    stack_frames: Vec<Frame>,
+    functions: HashMap<String, usize>,
+}
+
+#[derive(Debug)]
+struct Frame {
+    local_variables: HashMap<String, i64>,
+    return_address: usize,
+}
+
+impl VirtualMachine {
+    pub fn new(instructions: Vec<OpCode>) -> Self {
+        Self {
+            stack: vec![],
+            variables: HashMap::new(),
+            instructions,
+            instruction_pointer: 0,
+            call_stack: vec![],
+            stack_frames: vec![],
+            functions: HashMap::new(),
+        }
+    }
+
+    pub fn run(&mut self) {
+        while self.instruction_pointer < self.instructions.len() {
+            self.execute(&self.get_current_opcode().clone());
+        }
+    }
+
+    fn execute(&mut self, opcode: &OpCode) {
+        match opcode {
+            OpCode::PUSH(value) => self.stack.push(*value),
+            // OpCode::POP => {
+            //     self.stack.pop().expect("Stack underflow on POP");
+            // }
+            OpCode::PRINT => {
+                if let Some(value) = self.stack.pop() {
+                    println!("{}", value);
+                } else {
+                    panic!("Stack underflow on PRINT");
+                }
+            }
+
+            // Arithmetic
+            OpCode::ADD => self.binary_operation(|a, b| a + b),
+            OpCode::SUB => self.binary_operation(|a, b| a - b),
+            OpCode::MUL => self.binary_operation(|a, b| a * b),
+            OpCode::DIV => self.binary_operation(|a, b| a / b),
+
+            // Variable operations
+            OpCode::STORE(name) => {
+                let top_value = self.stack.pop().expect("Stack underflow on STORE");
+
+                // check if currently inside a function
+                if !self.stack_frames.is_empty() {
+                    // push variables to the function's local variables list
+                    self.stack_frames
+                        .last_mut()
+                        .unwrap()
+                        .local_variables
+                        .insert(name.clone(), top_value);
+                } else {
+                    self.variables.insert(name.clone(), top_value);
+                }
+            }
+            OpCode::LOAD(name) => {
+                let value = self
+                    .get_variable(name)
+                    .unwrap_or_else(|| panic!("Undefined variable: {}", name));
+                self.stack.push(value);
+            }
+
+            OpCode::DECLARE(name) => {
+                // skip declare opcode to go to enter opcode
+                self.functions
+                    .insert(name.clone(), self.instruction_pointer + 1);
+
+                // skip handle function until
+                while !matches!(self.instructions[self.instruction_pointer], OpCode::EXIT) {
+                    self.next_instruction();
+                }
+            }
+
+            // Function operations
+            OpCode::CALL(name) => {
+                let next_instruction = self.instruction_pointer + 1;
+                // Locate function and set up a new frame
+                let frame = Frame {
+                    local_variables: HashMap::new(),
+                    return_address: next_instruction,
+                };
+                self.stack_frames.push(frame);
+                println!("Allocate stack frame for function: {:?}", name);
+                // Jump to the function's start (implement function mapping logic)
+                self.call_stack.push(next_instruction);
+                self.instruction_pointer = self.find_function_start(name);
+            }
+            OpCode::TailCall(name) => {
+                // Tail call replaces the current frame
+                let frame = self
+                    .stack_frames
+                    .last_mut()
+                    .expect("No frame for tail call");
+                frame.local_variables.clear();
+                println!("Tail call optimize for function: {}", name);
+                // Jump to the function's start
+                self.instruction_pointer = self.find_function_start(name);
+            }
+            OpCode::RET => {
+                if let Some(frame) = self.stack_frames.pop() {
+                    self.instruction_pointer = frame.return_address;
+                    // skip jumping to the next instruction
+                    return;
+                } else {
+                    panic!("Return with no active frame");
+                }
+            }
+
+            OpCode::ENTER => {
+                self.stack_frames.last_mut().expect("No frame on ENTER");
+            }
+            OpCode::EXIT => {}
+
+            // Control Flow operations
+            OpCode::JUMP(address) => {
+                self.instruction_pointer = *address;
+                // skip jumping to the next instruction
+                return;
+            }
+            OpCode::JmpIfFalse(address) => {
+                if let Some(condition) = self.stack.pop() {
+                    if condition == 0 {
+                        self.instruction_pointer = *address;
+                        // skip jumping to the next instruction
+                        return;
+                    }
+                } else {
+                    panic!("Stack underflow on JmpIfFalse");
+                }
+            }
+            // OpCode::JmpIfTrue(address) => {
+            //     if let Some(condition) = self.stack.pop() {
+            //         if condition != 0 {
+            //             self.instruction_pointer = *address;
+            //             // skip jumping to the next instruction
+            //             return;
+            //         }
+            //     } else {
+            //         panic!("Stack underflow on JmpIfTrue");
+            //     }
+            // }
+
+            // Comparison operations
+            OpCode::EQUAL => self.binary_operation(|a, b| (a == b) as i64),
+            OpCode::NotEqual => self.binary_operation(|a, b| (a != b) as i64),
+        }
+
+        self.next_instruction();
+    }
+
+    fn binary_operation<F>(&mut self, op: F)
+    where
+        F: FnOnce(i64, i64) -> i64,
+    {
+        if let (Some(b), Some(a)) = (self.stack.pop(), self.stack.pop()) {
+            self.stack.push(op(a, b));
+        } else {
+            panic!("Stack underflow on binary operation");
+        }
+    }
+
+    fn find_function_start(&self, name: &String) -> usize {
+        *self
+            .functions
+            .get(name)
+            .unwrap_or_else(|| panic!("Undefined function name: {}", name))
+    }
+
+    fn get_variable(&self, name: &str) -> Option<i64> {
+        self.variables
+            .get(name)
+            .or_else(|| self.stack_frames.last()?.local_variables.get(name))
+            .copied()
+    }
+
+    fn get_current_opcode(&self) -> &OpCode {
+        &self.instructions[self.instruction_pointer]
+    }
+
+    fn next_instruction(&mut self) {
+        self.instruction_pointer += 1;
+    }
+}
